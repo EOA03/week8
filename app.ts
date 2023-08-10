bb835085690604173209030348e1ce004f865058
Ester <esteroa0310@gmail.com>
2023-08-10 21:11:59 +0700

    modified app.ts

--- app.ts
+++ app.ts
@@ -11,6 +11,10 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(routes)
 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Belajar Express");
+})
+
 app.listen(port, ()=>{
     console.log(`Server listen ${port}`);
 })
